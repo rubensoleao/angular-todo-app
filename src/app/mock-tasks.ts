863c50c1cb6fff16d74ee516184f4eb963909bab
rubensoleao <rubensoleao@gmail.com>
2021-10-09 12:26:09 -0300

    Added task-itm and service

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,8 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  { id: 1, text: 'Task 1', day: 'Jan 1', reminder: true },
+  { id: 2, text: 'Task 2', day: 'Jan 2', reminder: true },
+  { id: 3, text: 'Task 3', day: 'Jan 3', reminder: true },
+  { id: 4, text: 'Task 4', day: 'Jan 4', reminder: true },
+];
