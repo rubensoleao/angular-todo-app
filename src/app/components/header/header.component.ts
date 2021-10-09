d7ae385130a3495f10438bfd6b46475307569c91
rubensoleao <rubensoleao@gmail.com>
2021-10-09 15:45:19 -0300

    Show/Hide add form
    This shows the concepts of Subscriber

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UiService } from 'src/app/services/ui.service';
+import { Subject, Subscription } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task tracker';
+  showAddTask: boolean = false;
+  subscritpion: Subscription | null = null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private uiService: UiService) {
+    this.subscritpion = this.uiService.onToggle().subscribe((value) => {
+      this.showAddTask = value;
+    });
   }
 
-  toggleAddTask(){
-    console.log("toggle")
+  ngOnInit(): void {}
+
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
   }
 }
